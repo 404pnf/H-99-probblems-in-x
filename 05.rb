e88335b4a63416bd47726a8346021a465ddb86c9
404pnf <404pnf@exmaple.org>
2013-08-13 10:37:27 +0800

    ruby中string和array无法没什么可公用的供递归使用的方法？

--- 05.rb
+++ 05.rb
@@ -10,21 +10,43 @@ Prelude> myReverse [1,2,3,4]
 [4,3,2,1]
 =end
 
-def rest coll
-  coll[1..-1]
+def but_last coll
+  coll[0..-2]
 end
 
 def reverse1 coll
   coll.reverse
 end
 
+
+# 如果用递归，因为ruby的sting没有pop，且拼出来的结果默认是array
+# 因此没有方法写一个通用的
+=begin
 def reverse2 coll
   if coll.empty?
-    nil
+    []
   else
-    [coll[0]].concat(rest coll)
+    [coll[-1]].concat (reverse2 (but_last coll))
+  end
+end 
+=end #错误的，因为结果是array，如果输入是string的话，就不对
+
+def reverse2 coll
+  if coll.is_a? String
+    if coll.empty?
+      ''
+    else
+      coll[-1] << reverse2(but_last coll)
+    end
+  else
+    if coll.empty?
+      []
+    else
+      [coll[-1]].concat (reverse2 (but_last coll))
+    end
   end
 end
+      
 
 require 'test/unit'
 class TestMy < Test::Unit::TestCase
@@ -33,7 +55,7 @@ class TestMy < Test::Unit::TestCase
     assert_equal "!amanap ,lanac a ,nalp a ,nam A", reverse1("A man, a plan, a canal, panama!") 
   end
   def test_reverse2
-   # assert_equal [4,3,2,1], reverse2([1,2,3,4])
-    #assert_equal "!amanap ,lanac a ,nalp a ,nam A", reverse2("A man, a plan, a canal, panama!") 
+   assert_equal [4,3,2,1], reverse2([1,2,3,4])
+   assert_equal "!amanap ,lanac a ,nalp a ,nam A", reverse2("A man, a plan, a canal, panama!") 
   end
 end
---
+++ 06.rb
@@ -0,0 +1,42 @@
+=begin
+6 Problem 6
+(*) Find out whether a list is a palindrome. A palindrome can be read forward or backward; e.g. (x a m a x).
+
+Example in Haskell:
+
+*Main> isPalindrome [1,2,3]
+False
+*Main> isPalindrome "madamimadam"
+True
+*Main> isPalindrome [1,2,4,8,16,8,4,2,1]
+True
+Solutions
+=end
+
+def palindrome coll
+  coll == coll.reverse
+end
+
+def palindrome2 coll
+  size = coll.size
+  (0..size).each do |i|
+    unless coll[i] == coll[-(i+1)]
+      return false
+    end
+  end
+  true
+end
+
+require 'test/unit'
+class TestMy < Test::Unit::TestCase
+  def test_reverse1
+    assert_equal true, palindrome("madamimadam")
+    assert_equal false, palindrome([1,2,3])
+    assert_equal true, palindrome([1,2,4,8,16,8,4,2,1])
+  end
+  def test_reverse2
+    assert_equal true, palindrome2("madamimadam")
+    assert_equal false, palindrome2([1,2,3])
+    assert_equal true, palindrome2([1,2,4,8,16,8,4,2,1])
+  end
+end
