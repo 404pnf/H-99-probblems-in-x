30ec8eceac4f62687c1bd3326ed2e1cf397a64d4
404pnf <404pnf@exmaple.org>
2013-08-14 13:22:46 +0800

    还是用平行赋值来让代码更简单

--- 02.rb
+++ 02.rb
@@ -11,17 +11,30 @@ Prelude> myButLast [1,2,3,4]
 Prelude> myButLast ['a'..'z']
 'y'
 =end
-
+def bulit_in_but_last x
+  x[-2]
+end
+# 好方法
+def butlast x
+  *_, i, _ = x
+  i
+end
 def my_but_last x
   x.pop
   x.pop
 end
 def new_but_last x
   x.pop
-  x.last
+  #x.last
+  x[-1]
 end
 require 'test/unit'
 class TestTry < Test::Unit::TestCase
+  def test_butlast
+    assert_equal 1, butlast([1,2])
+    assert_equal 2, butlast([1,2,3])
+    assert_equal 4, butlast([1,3,4,5])
+  end
   def test_my_but_last
     assert_equal 1, my_but_last([1,2])
     assert_equal 2, my_but_last([1,2,3])
