30dbc3619548da33ca024c14112ffed859b0da5b
404pnf <404pnf@exmaple.org>
2013-08-14 13:19:44 +0800

    seq[-1]可以用在string也可以array上，seq.last只对array有作用； *splat, last = list 真棒

--- 01.rb
+++ 01.rb
@@ -1,24 +1,26 @@
-
+# 默认的方法
 def last_alias x
-  x.last
+  #x.last
+  x[-1] # 这个写法既可以用在array上也可以用在string上
 end
 
-def my_last list
-	*init, last = list
-	if init.empty?
-		last
-	else
-		list.shift
-		my_last list 
-	end
+# 好方法
+def mylast list
+  *_, x = list
+  x
 end
+# 当last返回是nil的时候，你并不知道为什么 ：）
+#2.0.0-rc1 :014 > [].last
+# => nil
+#2.0.0-rc1 :015 > [1,nil].last
+# => nil
 
 require 'test/unit'
 class TestMyLast < Test::Unit::TestCase
-  def test_my_last
-    assert_equal 1, my_last([1])
-    assert_equal 2, my_last([1,2])
-    assert_equal 5, my_last([1,3,4,5])
+  def test_mylast
+    assert_equal 1, mylast([1])
+    assert_equal 2, mylast([1,2])
+    assert_equal 5, mylast([1,3,4,5])
   end
   def test_last_alias
     assert_equal 1, last_alias([1])
--- 09.rb
+++ 09.rb
@@ -26,6 +26,10 @@ def pack arr
   r.delete_if { |i| i == []}
 end
 
+# 那个对于字符串的我没有做
+# 因为觉得和ruby不是太有关系
+# 特别是 haskell 的list必须是同类型的，因此才有这个需要
+# 要用ruby实现需要去判断所有元素都是string然后再做相应处理，意义不大
 require 'test/unit'
 class TestMy < Test::Unit::TestCase
   def test_pack
