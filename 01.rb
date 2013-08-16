93d0928c9ad447d9c369409690ee669eb5458216
404pnf <404pnf@exmaple.org>
2013-08-16 13:19:20 +0800

    加入了rubocop的配置文件

--- 01.rb
+++ 01.rb
@@ -1,6 +1,6 @@
 # 默认的方法
 def last_alias x
-  #x.last
+  # x.last
   x[-1] # 这个写法既可以用在array上也可以用在string上
 end
 
@@ -10,9 +10,9 @@ def mylast list
   x
 end
 # 当last返回是nil的时候，你并不知道为什么 ：）
-#2.0.0-rc1 :014 > [].last
+# 2.0.0-rc1 :014 > [].last
 # => nil
-#2.0.0-rc1 :015 > [1,nil].last
+# 2.0.0-rc1 :015 > [1,nil].last
 # => nil
 
 require 'test/unit'
@@ -22,6 +22,7 @@ class TestMyLast < Test::Unit::TestCase
     assert_equal 2, mylast([1,2])
     assert_equal 5, mylast([1,3,4,5])
   end
+
   def test_last_alias
     assert_equal 1, last_alias([1])
     assert_equal 2, last_alias([1,2])
--- 11.rb
+++ 11.rb
@@ -1,6 +1,5 @@
 =begin
-
-1 Problem 11
+1 Problem 11 
 (*) Modified run-length encoding.
 
 Modify the result of problem 10 in such a way that if an element has no duplicates it is simply copied into the result list. Only elements with duplicates are transferred as (N E) lists.
